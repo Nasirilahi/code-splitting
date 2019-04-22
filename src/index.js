e976f50e354fe9d49ec3cee04376425c81d5db2d
Nasir Husain <nasir.husain@tokopedia.com>
2019-04-22 13:58:42 +0530

    worked on splitting code based on entry points

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import _ from 'lodash'
 import cusLodash from './custom-lodash'
 import { add } from './math'
 
+cusLodash()
+
 function component() {
   let element = document.createElement('div')
-  element.innerHTML = _.join(['Hello', 'webpack'])
+  element.innerHTML = _.join(['Hello', 'webpack', ` ${add(10 ,20)}`])
   return element
 }
 
